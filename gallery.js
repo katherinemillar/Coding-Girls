a4d65d9fe5876714fea6119c626450b0f5794ed6
Katherine Millar <katiemillar88@gmail.com>
2021-03-22 16:45:52 +0000

    commit to 'main'

--- gallery.js
+++ gallery.js
@@ -1,6 +1,5 @@
 // "listens" for when the window is scrolled on. When a scroll happens, "scrollFunction" is executed
 $(window).scroll(scrollFunction);
-
 // When the user scrolls down 20px from the top of the document, show the button
 function scrollFunction() {
   if ( $(window).scrollTop() > 20 ) {
@@ -9,7 +8,6 @@ function scrollFunction() {
     $("#go-top-button").hide();
   }
 };
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   $(window).scrollTop(0);
